0eb613ead53b335ce04661e90b53b7b38bdc38c9
syafrin <syafrinibrahim12@gmail.com>
2020-09-02 22:22:42 -0700

    integrate dashboard view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transaction;
 
 class DashboardController extends Controller
 {
@@ -12,6 +13,14 @@ class DashboardController extends Controller
     }
     
     public function index(){
-        return view('pages.dashboard');
+        $income = Transaction::where('transaction_sts', 'SUCCESS')->sum('transaction_ttl');
+        $sales = Transaction::count();
+        $items = Transaction::orderBy('id','DESC')->take(5)->get();
+        $pie = [
+            'pending' => Transaction::where('transaction_sts','PENDING')->count(),
+            'success' => Transaction::where('transaction_sts','SUCCESS')->count(),
+            'failed' => Transaction::where('transaction_sts','FAILED')->count()
+        ];
+        return view('pages.dashboard',compact('income','sales','pie','items'));
     }
 }
